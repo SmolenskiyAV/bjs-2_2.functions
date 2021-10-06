d306282e667d455671bc59a152fd87d5954843ac
SmolenskiyAV <JoJo_666@list.ru>
2021-10-06 22:20:08 +0700

    Версия 1.0

--- task.js
+++ task.js
@@ -1,31 +1,68 @@
 // Задание 1
 function getArrayParams(arr) {
   let min,max,sum,avg;
+// ############ TASK 1 ####################
+    min = arr[0];
+    max = arr[0];
+    sum = 0;
+    let arrLength = arr.length;
+    for (i = 0; i < arrLength; i += 1) {
+      if (arr[i] < min) { //ищем минимум
+        min = arr[i];
+      }
+      if (arr[i] > max) {//ищем максимум
+        max = arr[i];
+      }
+      sum = sum + arr[i]; //вычисляем сумму элементов массмва arr[]
+    }
 
-  // Ваш код
+    avg = Number((sum / arrLength).toFixed(2)); //вычисляем среднее значение, обрезаем до 2-го знака после запятой, преобразуем в число
 
   return { min:min, max:max, avg:avg };
 }
 
-// Задание 2
+// ############# Задание 2 #################
 function worker(arr) {
-  let sum;
+  let sum = 0;
+  let arrLength = arr.length;
 
-  // Ваш код
+  for (i = 0; i < arrLength; i += 1) {
+    sum = sum + arr[i]; //вычисляем сумму элементов массмва arr[]
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
+  let max = -Infinity;
+  let arrOfArrLength = arrOfArr.length;
 
-  // Ваш кода
-  // for ...
-  
-  return max
+  for (i = 0; i < arrOfArrLength; i += 1) {
+      arrResultElement = func(arrOfArr[i]);
+      if (arrResultElement > max) {//ищем максимум
+        max = arrResultElement;
+      }
+      
+    }
+    
+  return max;
 }
 
-// Задание 3
+// ############### Задание 3 ###############
 function worker2(arr) {
-  // Ваш код
+  let min = arr[0];
+   let max = arr[0];
+   let arrLength = arr.length;
+    for (i = 0; i < arrLength; i += 1) {
+      if (arr[i] < min) { //ищем минимум
+        min = arr[i]; 
+      }
+      if (arr[i] > max) {//ищем максимум
+        max = arr[i]; 
+      }
+    }
+
+    let delta = Math.abs(max - min); //вычисляем расстояние между мин и макс элементами массива arr[]
+    
+    return delta;
 }
